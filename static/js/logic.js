3f6261ec8a3026f6930533e3713457a664069784
Yanhuan Huang <hyhuan0318@gmail.com>
2019-10-04 21:26:19 -0400

    updates

---
+++ static/js/logic.js
@@ -0,0 +1,6 @@
+// Loop through names in samples.json to fill in dropdown options
+    // var options = ""
+    // for(var i = 0; i < d.names.length; i++) {
+    //     options += `<option value="${i}">`+ d.names[i] +"</option>";
+    //   }
+    //   document.getElementById("selDataset").innerHTML = options;
